f89ae12414c04cca9ad2b395d46375ed109efbd7
Benjamin Mmari <benmmari92@gmail.com>
2018-03-13 13:38:29 +0200

    complete second function

--- src/service.py
+++ src/service.py
@@ -1,9 +1,12 @@
 from nameko.rpc import rpc
 import math
+import zlib
 
 
 class AssessmentService(object):
     name = 'assessment_service'
+
+    string_dictionary = {}
 
     @rpc
     def one(self, number_list):
@@ -11,3 +14,9 @@ class AssessmentService(object):
         for number in number_list:
             squared_number_list.append(int(math.pow(number, 2)))
         return squared_number_list
+
+    @rpc
+    def two(self, string_list):
+        for string in string_list:
+            self.string_dictionary[string] = zlib.compress(string)
+        return self.string_dictionary
